c32ed049d44557de7dace4e193d5c55e51c97c2d
YourCodingBench <snapshot@yourcodingbench.com>
2026-08-22 07:06:02 +0000

    ycb snapshot: orphan @ 1454d2bc0f378d7f34766afafee68a77e7b85995

---
+++ cmd/pebble/db.go
@@ -0,0 +1,181 @@
+// Copyright 2019 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/cockroachdb/pebble"
+	"github.com/cockroachdb/pebble/cockroachkvs"
+	"github.com/cockroachdb/pebble/internal/bytealloc"
+	"github.com/cockroachdb/pebble/objstorage/remote"
+	"github.com/cockroachdb/pebble/sstable"
+	"github.com/cockroachdb/pebble/vfs"
+)
+
+// DB specifies the minimal interfaces that need to be implemented to support
+// the pebble command.
+type DB interface {
+	NewIter(*pebble.IterOptions) iterator
+	NewBatch() batch
+	Scan(iter iterator, key []byte, count int64, reverse bool) error
+	Metrics() *pebble.Metrics
+	Flush() error
+}
+
+type iterator interface {
+	SeekLT(key []byte) bool
+	SeekGE(key []byte) bool
+	Valid() bool
+	Key() []byte
+	Value() []byte
+	First() bool
+	Next() bool
+	Last() bool
+	Prev() bool
+	Close() error
+}
+
+type batch interface {
+	Close() error
+	Commit(opts *pebble.WriteOptions) error
+	Set(key, value []byte, opts *pebble.WriteOptions) error
+	Delete(key []byte, opts *pebble.WriteOptions) error
+	LogData(data []byte, opts *pebble.WriteOptions) error
+}
+
+// Adapters for Pebble. Since the interfaces above are based on Pebble's
+// interfaces, it can simply forward calls for everything.
+type pebbleDB struct {
+	d       *pebble.DB
+	ballast []byte
+}
+
+func newPebbleDB(dir string) DB {
+	opts := &pebble.Options{
+		CacheSize:                   cacheSize,
+		Comparer:                    &cockroachkvs.Comparer,
+		DisableWAL:                  disableWAL,
+		FormatMajorVersion:          pebble.FormatNewest,
+		KeySchema:                   cockroachkvs.KeySchema.Name,
+		KeySchemas:                  sstable.MakeKeySchemas(&cockroachkvs.KeySchema),
+		L0CompactionThreshold:       2,
+		L0StopWritesThreshold:       1000,
+		LBaseMaxBytes:               64 << 20, // 64 MB
+		MaxOpenFiles:                16384,
+		MemTableSize:                64 << 20,
+		MemTableStopWritesThreshold: 4,
+		Merger: &pebble.Merger{
+			Name: "cockroach_merge_operator",
+		},
+		CompactionConcurrencyRange: func() (int, int) {
+			return 1, 3
+		},
+	}
+	// Enable value separation. Note the minimum size of 512 means that only the
+	// variant of the ycsb benchmarks that uses 1024 values will result in any
+	// value separation.
+	opts.Experimental.ValueSeparationPolicy = func() pebble.ValueSeparationPolicy {
+		return pebble.ValueSeparationPolicy{
+			Enabled:                  true,
+			MinimumSize:              512,
+			MinimumMVCCGarbageSize:   32,
+			MaxBlobReferenceDepth:    10,
+			RewriteMinimumAge:        5 * time.Minute,
+			GarbageRatioLowPriority:  0.10, // 10% garbage
+			GarbageRatioHighPriority: 0.30, // 30% garbage
+		}
+	}
+
+	// Running the tool should not start compactions due to garbage.
+	opts.Experimental.CompactionGarbageFractionForMaxConcurrency = func() float64 {
+		return -1.0
+	}
+	for i := 0; i < len(opts.Levels); i++ {
+		l := &opts.Levels[i]
+		l.BlockSize = 32 << 10       // 32 KB
+		l.IndexBlockSize = 256 << 10 // 256 KB
+	}
+	opts.ApplyTableFilterPolicy(func() pebble.DBTableFilterPolicy { return pebble.DBTableFilterPolicyProgressive })
+	opts.FlushSplitBytes = opts.TargetFileSizes[0]
+
+	opts.EnsureDefaults()
+
+	if verbose {
+		lel := pebble.MakeLoggingEventListener(nil)
+		opts.EventListener = &lel
+		opts.EventListener.TableDeleted = nil
+		opts.EventListener.TableIngested = nil
+		opts.EventListener.WALCreated = nil
+		opts.EventListener.WALDeleted = nil
+	}
+
+	if pathToLocalSharedStorage != "" {
+		opts.Experimental.RemoteStorage = remote.MakeSimpleFactory(map[remote.Locator]remote.Storage{
+			// Store all shared objects on local disk, for convenience.
+			"": remote.NewLocalFS(pathToLocalSharedStorage, vfs.Default),
+		})
+		opts.Experimental.CreateOnShared = remote.CreateOnSharedAll
+		if secondaryCacheSize != 0 {
+			opts.Experimental.SecondaryCacheSizeBytes = secondaryCacheSize
+		}
+	}
+
+	p, err := pebble.Open(dir, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if pathToLocalSharedStorage != "" {
+		if err := p.SetCreatorID(1); err != nil {
+			log.Fatal(err)
+		}
+	}
+	return pebbleDB{
+		d:       p,
+		ballast: make([]byte, 1<<30),
+	}
+}
+
+func (p pebbleDB) Flush() error {
+	return p.d.Flush()
+}
+
+func (p pebbleDB) NewIter(opts *pebble.IterOptions) iterator {
+	iter, _ := p.d.NewIter(opts)
+	return iter
+}
+
+func (p pebbleDB) NewBatch() batch {
+	return p.d.NewBatch()
+}
+
+func (p pebbleDB) Scan(iter iterator, key []byte, count int64, reverse bool) error {
+	var data bytealloc.A
+	if reverse {
+		for i, valid := 0, iter.SeekLT(key); valid; valid = iter.Prev() {
+			data, _ = data.Copy(iter.Key())
+			data, _ = data.Copy(iter.Value())
+			i++
+			if i >= int(count) {
+				break
+			}
+		}
+	} else {
+		for i, valid := 0, iter.SeekGE(key); valid; valid = iter.Next() {
+			data, _ = data.Copy(iter.Key())
+			data, _ = data.Copy(iter.Value())
+			i++
+			if i >= int(count) {
+				break
+			}
+		}
+	}
+	return nil
+}
+
+func (p pebbleDB) Metrics() *pebble.Metrics {
+	return p.d.Metrics()
+}
---
+++ sstable/colblk/data_block_test.go
@@ -0,0 +1,246 @@
+// Copyright 2024 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package colblk
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand/v2"
+	"slices"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/crlib/crstrings"
+	"github.com/cockroachdb/datadriven"
+	"github.com/cockroachdb/pebble/internal/base"
+	"github.com/cockroachdb/pebble/internal/binfmt"
+	"github.com/cockroachdb/pebble/internal/itertest"
+	"github.com/cockroachdb/pebble/internal/testkeys"
+	"github.com/cockroachdb/pebble/internal/treeprinter"
+	"github.com/cockroachdb/pebble/sstable/block"
+	"github.com/cockroachdb/pebble/sstable/blockiter"
+)
+
+var testKeysSchema = DefaultKeySchema(testkeys.Comparer, 16)
+
+func TestDataBlock(t *testing.T) {
+	var buf bytes.Buffer
+	var w DataBlockEncoder
+	var r DataBlockDecoder
+	var bd BlockDecoder
+	var v DataBlockValidator
+	var it DataBlockIter
+	tieringConfig := NoTieringColumns()
+	rw := NewDataBlockRewriter(&testKeysSchema, testkeys.Comparer.EnsureDefaults(), tieringConfig)
+	var sizes []int
+	it.InitOnce(&testKeysSchema, testkeys.Comparer,
+		getInternalValuer(func([]byte) base.InternalValue {
+			return base.MakeInPlaceValue([]byte("mock external value"))
+		}), tieringConfig)
+
+	datadriven.Walk(t, "testdata/data_block", func(t *testing.T, path string) {
+		datadriven.RunTest(t, path, func(t *testing.T, td *datadriven.TestData) string {
+			buf.Reset()
+			switch td.Cmd {
+			case "init":
+				var bundleSize int
+				if td.MaybeScanArgs(t, "bundle-size", &bundleSize) {
+					s := DefaultKeySchema(testkeys.Comparer, bundleSize)
+					w.Init(&s, NoTieringColumns())
+				} else {
+					w.Init(&testKeysSchema, NoTieringColumns())
+				}
+				fmt.Fprint(&buf, &w)
+				sizes = sizes[:0]
+				return buf.String()
+			case "write", "write-block":
+				// write-block does init/write/finish in a single command, and doesn't
+				// print anything.
+				if td.Cmd == "write-block" {
+					w.Init(&testKeysSchema, NoTieringColumns())
+				}
+				var prevKey base.InternalKey
+				for line := range crstrings.LinesSeq(td.Input) {
+					line, isObsolete := strings.CutSuffix(line, "obsolete")
+
+					j := strings.IndexRune(line, ':')
+					ik := base.ParseInternalKey(line[:j])
+
+					kcmp := w.KeyWriter.ComparePrev(ik.UserKey)
+					valueString := line[j+1:]
+					vp := block.InPlaceValuePrefix(kcmp.PrefixEqual())
+					if strings.HasPrefix(valueString, "valueHandle") {
+						vp = block.ValueBlockHandlePrefix(kcmp.PrefixEqual(), 0)
+					} else if strings.HasPrefix(valueString, "blobHandle") {
+						vp = block.BlobValueHandlePrefix(kcmp.PrefixEqual(), 0)
+					}
+					if kcmp.UserKeyComparison == 0 && prevKey.Kind() != base.InternalKeyKindMerge {
+						isObsolete = true
+					}
+					v := []byte(line[j+1:])
+					w.Add(ik, v, vp, kcmp, isObsolete, base.KVMeta{})
+					prevKey = ik
+					sizes = append(sizes, w.Size())
+				}
+				if td.Cmd == "write-block" {
+					block, _ := w.Finish(w.Rows(), w.Size())
+					bd = r.Init(&testKeysSchema, block)
+					return ""
+				}
+				fmt.Fprint(&buf, &w)
+				return buf.String()
+			case "rewrite":
+				var from, to string
+				td.ScanArgs(t, "from", &from)
+				td.ScanArgs(t, "to", &to)
+				start, end, rewrittenBlock, err := rw.RewriteSuffixes(bd.Data(), []byte(from), []byte(to))
+				if err != nil {
+					return fmt.Sprintf("error: %s", err)
+				}
+				bd = r.Init(&testKeysSchema, rewrittenBlock)
+				f := binfmt.New(bd.Data()).LineWidth(20)
+				tp := treeprinter.New()
+				r.Describe(f, tp, bd)
+				fmt.Fprintf(&buf, "Start: %s\nEnd: %s\n%s",
+					start.Pretty(testkeys.Comparer.FormatKey),
+					end.Pretty(testkeys.Comparer.FormatKey),
+					tp.String())
+				return buf.String()
+			case "finish":
+				rows := w.Rows()
+				td.MaybeScanArgs(t, "rows", &rows)
+				block, lastKey := w.Finish(rows, sizes[rows-1])
+				bd = r.Init(&testKeysSchema, block)
+				f := binfmt.New(bd.Data()).LineWidth(20)
+				tp := treeprinter.New()
+				r.Describe(f, tp, bd)
+				fmt.Fprintf(&buf, "LastKey: %s\n%s", lastKey.Pretty(testkeys.Comparer.FormatKey), tp.String())
+				if err := v.Validate(block, testkeys.Comparer, &testKeysSchema); err != nil {
+					fmt.Fprintln(&buf, err)
+				}
+				return buf.String()
+			case "iter":
+				var seqNum uint64
+				var syntheticPrefix, syntheticSuffix string
+				td.MaybeScanArgs(t, "synthetic-seq-num", &seqNum)
+				td.MaybeScanArgs(t, "synthetic-prefix", &syntheticPrefix)
+				td.MaybeScanArgs(t, "synthetic-suffix", &syntheticSuffix)
+				transforms := blockiter.Transforms{
+					SyntheticSeqNum:          blockiter.SyntheticSeqNum(seqNum),
+					HideObsoletePoints:       td.HasArg("hide-obsolete-points"),
+					SyntheticPrefixAndSuffix: blockiter.MakeSyntheticPrefixAndSuffix([]byte(syntheticPrefix), []byte(syntheticSuffix)),
+				}
+				if err := it.Init(&r, bd, transforms, tieringConfig); err != nil {
+					return err.Error()
+				}
+
+				o := []itertest.IterOpt{itertest.ShowCommands}
+				if td.HasArg("verbose") {
+					o = append(o, itertest.Verbose)
+				}
+				if td.HasArg("invalidated") {
+					it.Invalidate()
+				}
+				return itertest.RunInternalIterCmd(t, td, &it, o...)
+			default:
+				return fmt.Sprintf("unknown command: %s", td.Cmd)
+			}
+		})
+	})
+}
+
+func BenchmarkDataBlockWriter(b *testing.B) {
+	for _, prefixSize := range []int{8, 32, 128} {
+		for _, valueSize := range []int{8, 128, 1024} {
+			b.Run(fmt.Sprintf("prefix=%d,value=%d", prefixSize, valueSize), func(b *testing.B) {
+				benchmarkDataBlockWriter(b, prefixSize, valueSize)
+			})
+		}
+	}
+}
+
+func benchmarkDataBlockWriter(b *testing.B, prefixSize, valueSize int) {
+	const targetBlockSize = 32 << 10
+	seed := uint64(time.Now().UnixNano())
+	rng := rand.New(rand.NewPCG(0, seed))
+	keys, values := makeTestKeyRandomKVs(rng, prefixSize, valueSize, targetBlockSize)
+
+	var w DataBlockEncoder
+	w.Init(&testKeysSchema, NoTieringColumns())
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		w.Reset()
+		var j int
+		for w.Size() < targetBlockSize {
+			ik := base.MakeInternalKey(keys[j], base.SeqNum(rng.Uint64N(uint64(base.SeqNumMax))), base.InternalKeyKindSet)
+			kcmp := w.KeyWriter.ComparePrev(ik.UserKey)
+			vp := block.InPlaceValuePrefix(kcmp.PrefixEqual())
+			w.Add(ik, values[j], vp, kcmp, false /* isObsolete */, base.KVMeta{})
+			j++
+		}
+		w.Finish(w.Rows(), w.Size())
+	}
+}
+
+func makeTestKeyRandomKVs(
+	rng *rand.Rand, prefixSize, valueSize int, aggregateSize int,
+) (keys, vals [][]byte) {
+	keys = make([][]byte, aggregateSize/valueSize+1)
+	vals = make([][]byte, len(keys))
+	for i := range keys {
+		keys[i] = randTestKey(rng, make([]byte, prefixSize+testkeys.MaxSuffixLen), prefixSize)
+		vals[i] = make([]byte, valueSize)
+		for j := range vals[i] {
+			vals[i][j] = byte(rng.Uint32())
+		}
+	}
+	slices.SortFunc(keys, bytes.Compare)
+	return keys, vals
+}
+
+func randTestKey(rng *rand.Rand, buf []byte, prefixLen int) []byte {
+	suffix := rng.Int64N(100)
+	sl := testkeys.SuffixLen(suffix)
+	buf = buf[0 : prefixLen+sl]
+	for i := 0; i < prefixLen; i++ {
+		buf[i] = byte(rng.IntN(26) + 'a')
+	}
+	testkeys.WriteSuffix(buf[prefixLen:], suffix)
+	return buf
+}
+
+type getInternalValuer func([]byte) base.InternalValue
+
+func (g getInternalValuer) GetInternalValueForPrefixAndValueHandle(
+	handle []byte,
+) base.InternalValue {
+	return g(handle)
+}
+
+func BenchmarkDataBlockDecoderInit(b *testing.B) {
+	const targetBlockSize = 32 << 10
+	seed := uint64(20250919)
+	rng := rand.New(rand.NewPCG(0, seed))
+	keys, values := makeTestKeyRandomKVs(rng, 8, 8, targetBlockSize)
+
+	var w DataBlockEncoder
+	w.Init(&testKeysSchema, NoTieringColumns())
+	for j := 0; w.Size() < targetBlockSize; j++ {
+		ik := base.MakeInternalKey(keys[j], base.SeqNum(rng.Uint64N(uint64(base.SeqNumMax))), base.InternalKeyKindSet)
+		kcmp := w.KeyWriter.ComparePrev(ik.UserKey)
+		vp := block.InPlaceValuePrefix(kcmp.PrefixEqual())
+		w.Add(ik, values[j], vp, kcmp, false /* isObsolete */, base.KVMeta{})
+	}
+	finished, _ := w.Finish(w.Rows(), w.Size())
+
+	var md block.Metadata
+
+	b.ResetTimer()
+	for range b.N {
+		InitDataBlockMetadata(&testKeysSchema, &md, finished)
+	}
+}
