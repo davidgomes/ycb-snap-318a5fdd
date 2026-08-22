456da3a2fcb417df7f1bb22382036b0eb143ac56
YourCodingBench <snapshot@yourcodingbench.com>
2026-08-22 07:06:25 +0000

    ycb snapshot: orphan @ 1047cdc393fac7c98822c993d70c28f58833c63d

---
+++ packages/vue-query/src/__tests__/useIsMutating.test.ts
@@ -0,0 +1,111 @@
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest'
+import { onScopeDispose, reactive, ref } from 'vue-demi'
+import { sleep } from '@tanstack/query-test-utils'
+import { useMutation } from '../useMutation'
+import { useIsMutating } from '../useMutationState'
+import type { MockedFunction } from 'vitest'
+
+vi.mock('../useQueryClient')
+
+describe('useIsMutating', () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  test('should properly return isMutating state', async () => {
+    const mutation = useMutation({
+      mutationFn: (params: string) => sleep(10).then(() => params),
+    })
+    const mutation2 = useMutation({
+      mutationFn: (params: string) => sleep(10).then(() => params),
+    })
+    const isMutating = useIsMutating()
+
+    expect(isMutating.value).toStrictEqual(0)
+
+    mutation.mutateAsync('a')
+    mutation2.mutateAsync('b')
+
+    await vi.advanceTimersByTimeAsync(0)
+
+    expect(isMutating.value).toStrictEqual(2)
+
+    await vi.advanceTimersByTimeAsync(10)
+
+    expect(isMutating.value).toStrictEqual(0)
+  })
+
+  test('should stop listening to changes on onScopeDispose', async () => {
+    const onScopeDisposeMock = onScopeDispose as MockedFunction<
+      typeof onScopeDispose
+    >
+    onScopeDisposeMock.mockImplementation((fn) => fn())
+
+    const mutation = useMutation({
+      mutationFn: (params: string) => sleep(0).then(() => params),
+    })
+    const mutation2 = useMutation({
+      mutationFn: (params: string) => sleep(0).then(() => params),
+    })
+    const isMutating = useIsMutating()
+
+    expect(isMutating.value).toStrictEqual(0)
+
+    mutation.mutateAsync('a')
+    mutation2.mutateAsync('b')
+
+    await vi.advanceTimersByTimeAsync(0)
+
+    expect(isMutating.value).toStrictEqual(0)
+
+    await vi.advanceTimersByTimeAsync(0)
+
+    expect(isMutating.value).toStrictEqual(0)
+
+    onScopeDisposeMock.mockReset()
+  })
+
+  test('should properly update filters', async () => {
+    const filter = reactive({ mutationKey: ['foo'] })
+    const { mutate } = useMutation({
+      mutationKey: ['isMutating'],
+      mutationFn: (params: string) => sleep(10).then(() => params),
+    })
+    mutate('foo')
+
+    const isMutating = useIsMutating(filter)
+
+    expect(isMutating.value).toStrictEqual(0)
+
+    filter.mutationKey = ['isMutating']
+
+    await vi.advanceTimersByTimeAsync(0)
+
+    expect(isMutating.value).toStrictEqual(1)
+  })
+
+  test('should work with options getter and be reactive', async () => {
+    const keyRef = ref('isMutatingGetter2')
+    const { mutate } = useMutation({
+      mutationKey: ['isMutatingGetter'],
+      mutationFn: (params: string) => sleep(10).then(() => params),
+    })
+    mutate('foo')
+
+    const isMutating = useIsMutating(() => ({
+      mutationKey: [keyRef.value],
+    }))
+
+    expect(isMutating.value).toStrictEqual(0)
+
+    keyRef.value = 'isMutatingGetter'
+
+    await vi.advanceTimersByTimeAsync(0)
+
+    expect(isMutating.value).toStrictEqual(1)
+  })
+})
